3e06bddbf19265b2f897a3eeb8ecc6589455defb
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-07-12 21:41:53 +0300

    GHF

--- app/src/pages/application/okPage.tsx
+++ app/src/pages/application/okPage.tsx
@@ -8,8 +8,19 @@ export default function OkPage() {
                     <h1 className={'ok--container--content--heading--title'}>Сервис с использованием API <span className={'ok--container--content--heading--title__ok'}>Одноклассники</span></h1>
                     <Link to={'/'} className={'ok--container--content--heading--link'}>На главную</Link>
                 </div>
+                <div className={'line'}/>
                 <div className={'ok--container--content'}>
-
+                    <form className={'ok--container--content--form'}>
+                        <input type="text" className={'ok--container--content--form__input'}
+                               placeholder={'Введите ваш id'}
+                        />
+                        <input type="text" className={'ok--container--content--form__input'}
+                               placeholder={'Введите id вашего друга'}
+                        />
+                        <input type="button" className={'ok--container--content--form__submit'}
+                               value={'Подтвердить'}
+                        />
+                    </form>
                 </div>
             </div>
         </section>
--- app/src/pages/application/vkPage.tsx
+++ app/src/pages/application/vkPage.tsx
@@ -8,8 +8,19 @@ export default function VkPage() {
                     <h1 className={'vk--container--content--heading--title'}>Сервис с использованием API <span className={'vk--container--content--heading--title__vk'}>Вконтакте</span></h1>
                     <Link to={'/'} className={'vk--container--content--heading--link'}>На главную</Link>
                 </div>
+                <div className={'line'}/>
                 <div className={'vk--container--content'}>
-
+                    <form className={'vk--container--content--form'}>
+                        <input type="text" className={'vk--container--content--form__input'}
+                               placeholder={'Введите ваш id'}
+                        />
+                        <input type="text" className={'vk--container--content--form__input'}
+                               placeholder={'Введите id вашего друга'}
+                        />
+                        <input type="button" className={'vk--container--content--form__submit'}
+                               value={'Подтвердить'}
+                        />
+                    </form>
                 </div>
             </div>
         </section>
